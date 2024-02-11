e236e6e8e04bc46359c4cdd7ab1229a44b4d4b13
Boyan Bogdanov <boyan.bogdan@gmail.com>
2024-02-12 00:12:32 +0200

    feat: naive classification- diff in mu sums

--- model/fuzzy/main.py
+++ model/fuzzy/main.py
@@ -6,7 +6,7 @@ from pprint import pprint
 # display one team's fitness as product of its μ fields
 # 1. teams json
 # 2. μ calculator
-# 3. predict outcomes via fit(team)
+# 3. predict outcomes via diff in fit(team) vs actual outcomes 
 
 df = pd.read_json('../../data-generation/out/training_data/teams-2022-2023.json')
 thresholds = pd.read_json('./thresholds.json')
@@ -29,11 +29,33 @@ for index, row in df.iterrows():
     teams.append(team)
 
 
-scores = []
+scores = {}
 for team in teams:
     team_score = 0
     for stat in team["stats"]:
         team_score += stat.mu
-    scores.append((team["name"], team_score))
+    scores[team["name"]] =  team_score
 
 print(scores)
+
+outcomes = pd.read_json('../../data-generation/out/training_data/sample-2022-2023.json')
+
+accuracy, count = 0, 0
+for i in outcomes.index:
+    count += 1
+    outcome = outcomes['matchOutcome'][i]
+    team_one_name = outcomes['team_one_name'][i]
+    team_two_name = outcomes['team_two_name'][i]
+    fit_one, fit_two = scores[team_one_name], scores[team_two_name]
+    predict = 1
+    if fit_one - fit_two > 0.4:
+        predict = 2
+    if fit_two - fit_one > 0.4:
+        predict = 0
+    
+    if predict == outcome:
+        accuracy += 1
+
+accuracy /= count
+
+print(accuracy)
