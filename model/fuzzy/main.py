c2912e19eeb70b236299f83118560136ae09d28f
Boyan Bogdanov <boyan.bogdan@gmail.com>
2024-02-12 01:02:54 +0200

    feat: adjust scoring, add accuracy notes

--- model/fuzzy/main.py
+++ model/fuzzy/main.py
@@ -1,7 +1,8 @@
+import math
+from numpy import Infinity
 import pandas as pd
 
 from FuzzyElement import FuzzyElement
-from pprint import pprint
 
 # display one team's fitness as product of its μ fields
 # 1. teams json
@@ -31,16 +32,20 @@ for index, row in df.iterrows():
 
 scores = {}
 for team in teams:
-    team_score = 0
+    team_score = pow(10,6)
     for stat in team["stats"]:
-        team_score += stat.mu
-    scores[team["name"]] =  team_score
+        if stat.mu == 0:
+            print(stat.name, team["name"])
+        team_score *= stat.mu
+
+    scores[team["name"]] = math.log2(team_score)
 
 print(scores)
 
 outcomes = pd.read_json('../../data-generation/out/training_data/sample-2022-2023.json')
 
 accuracy, count = 0, 0
+diff_threshold = 0
 for i in outcomes.index:
     count += 1
     outcome = outcomes['matchOutcome'][i]
@@ -48,14 +53,18 @@ for i in outcomes.index:
     team_two_name = outcomes['team_two_name'][i]
     fit_one, fit_two = scores[team_one_name], scores[team_two_name]
     predict = 1
-    if fit_one - fit_two > 0.4:
+    if fit_one - fit_two > diff_threshold:
         predict = 2
-    if fit_two - fit_one > 0.4:
+    if fit_two - fit_one > diff_threshold:
         predict = 0
     
     if predict == outcome:
         accuracy += 1
-
 accuracy /= count
 
 print(accuracy)
+
+# 31 draws, 159 wins/draws
+# 0.1631578947368421 accuracy if we always assume draw
+# 0.42 accuracy if we assume win/loss at random
+# 0.5894736842105263 accuracy if we always assume better fit wins where fit=log(product of mu) and no special weights for mu; for any threshold > 0 results get worse 
